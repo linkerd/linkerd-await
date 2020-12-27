6ad43d980d3c1cde3dec89e3815b2dd1d6623b4e
Oliver Gould <ver@buoyant.io>
2020-12-27 23:41:11 +0000

    Add a --shutdown mode
    When the --shutdown flag is provided, the specified command is forked
    (instead of simply exec'd) and the linkerd-await program forwards
    SIGTERM to the child process. When the child process completes, the
    proxy is shutdown via its admin interface.
    
    Depends on https://github.com/linkerd/linkerd2-proxy/pull/811

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![deny(warnings, rust_2018_idioms)]
 
-use std::time::Duration;
-use std::{error, fmt};
+use regex::Regex;
+use std::{convert::TryInto, error, fmt};
 use structopt::StructOpt;
-use tokio::time::delay_for;
+use tokio::time::{delay_for, Duration};
 
 #[derive(Clone, Debug, StructOpt)]
 #[structopt()]
@@ -11,10 +11,10 @@ use tokio::time::delay_for;
 struct Opt {
     #[structopt(
         short = "u",
-        long = "uri",
-        default_value = "http://127.0.0.1:4191/ready"
+        long = "base-url",
+        default_value = "http://127.0.0.1:4191/"
     )]
-    uri: http::Uri,
+    base_url: http::Uri,
 
     #[structopt(
         short = "b",
@@ -24,16 +24,25 @@ struct Opt {
     )]
     backoff: Duration,
 
+    #[structopt(
+        short = "S",
+        long = "shutdown",
+        help = "Causes the program to be forked so that proxy shutdown can be triggered once it completes"
+    )]
+    shutdown: bool,
+
     #[structopt(name = "CMD")]
     cmd: Vec<String>,
 }
 
 #[tokio::main]
 async fn main() {
-    use std::os::unix::process::CommandExt;
-    use std::process::{self, Command};
-
-    let Opt { uri, backoff, cmd } = Opt::from_args();
+    let Opt {
+        base_url,
+        backoff,
+        shutdown,
+        cmd,
+    } = Opt::from_args();
 
     let disabled_reason = std::env::var("LINKERD_DISABLED")
         .ok()
@@ -41,22 +50,97 @@ async fn main() {
     match disabled_reason {
         Some(reason) => eprintln!("Linkerd readiness check skipped: {}", reason),
         None => {
-            await_ready(uri, backoff).await;
+            await_ready(base_url.clone(), backoff).await;
         }
     }
 
     let mut args = cmd.into_iter();
     if let Some(command) = args.next() {
-        let mut cmd = Command::new(&command);
-        cmd.args(args);
-
-        let err = cmd.exec();
-        eprintln!("Failed to exec child program: {}: {}", command, err);
-        process::exit(1);
+        if shutdown {
+            let res = fork_and_wait(command, args).await;
+            send_shutdown(base_url).await;
+            if let Ok(status) = res {
+                if let Some(code) = status.code() {
+                    std::process::exit(code);
+                }
+            }
+            std::process::exit(EX_OSERR);
+        } else {
+            exec(command, args);
+        }
     }
 }
 
-async fn await_ready(uri: http::Uri, backoff: Duration) {
+const EX_OSERR: i32 = 71;
+
+fn exec(name: String, args: impl IntoIterator<Item = String>) {
+    use std::{
+        os::unix::process::CommandExt,
+        process::{self, Command},
+    };
+
+    let mut cmd = Command::new(&name);
+    cmd.args(args);
+
+    let err = cmd.exec();
+    eprintln!("Failed to exec child program: {}: {}", name, err);
+    process::exit(EX_OSERR);
+}
+
+#[allow(warnings)]
+async fn fork_and_wait(
+    name: String,
+    args: impl IntoIterator<Item = String>,
+) -> std::io::Result<std::process::ExitStatus> {
+    use tokio::{
+        process::Command,
+        signal::unix::{signal, SignalKind},
+    };
+
+    let mut cmd = Command::new(&name);
+    cmd.args(args);
+
+    let child = match cmd.spawn() {
+        Ok(child) => child,
+        Err(e) => {
+            eprintln!("Failed to fork child program: {}: {}", name, e);
+            std::process::exit(EX_OSERR);
+        }
+    };
+
+    // Proxy relevant signals.
+    let cid = nix::unistd::Pid::from_raw(child.id().try_into().expect("Invalid PID"));
+    tokio::spawn(async move {
+        // SIGINT  - To allow Ctrl-c to emulate SIGTERM while developing.
+        let mut sigint =
+            signal(SignalKind::interrupt()).expect("Failed to register signal handler");
+        // SIGTERM - Kubernetes sends this to start a graceful shutdown.
+        let mut sigterm =
+            signal(SignalKind::terminate()).expect("Failed to register signal handler");
+        tokio::select! {
+            _ = sigint.recv() => {
+                if let Err(e) = nix::sys::signal::kill(cid, nix::sys::signal::Signal::SIGINT) {
+                    eprintln!("Failed to forward SIGINT to child process: {}", e);
+                }
+            }
+            _ = sigterm.recv() => {
+                if let Err(e) = nix::sys::signal::kill(cid, nix::sys::signal::Signal::SIGTERM) {
+                    eprintln!("Failed to forward SIGTERM to child process: {}", e);
+                }
+            }
+        };
+    });
+
+    child.await
+}
+
+async fn await_ready(base_url: http::Uri, backoff: Duration) {
+    let uri = {
+        let mut parts = base_url.into_parts();
+        parts.path_and_query = Some("/ready".try_into().unwrap());
+        http::Uri::from_parts(parts).expect("Ready URI must be valid")
+    };
+
     let client = hyper::Client::default();
     loop {
         match client.get(uri.clone()).await {
@@ -66,9 +150,23 @@ async fn await_ready(uri: http::Uri, backoff: Duration) {
     }
 }
 
-fn parse_duration(s: &str) -> Result<Duration, InvalidDuration> {
-    use regex::Regex;
+async fn send_shutdown(base_url: http::Uri) {
+    let uri = {
+        let mut parts = base_url.into_parts();
+        parts.path_and_query = Some("/shutdown".try_into().unwrap());
+        http::Uri::from_parts(parts).expect("Shutdown URI must be valid")
+    };
 
+    let req = http::Request::builder()
+        .method(http::Method::POST)
+        .uri(uri)
+        .body(Default::default())
+        .expect("shutdown request must be valid");
+
+    let _ = hyper::Client::default().request(req).await;
+}
+
+fn parse_duration(s: &str) -> Result<Duration, InvalidDuration> {
     let re = Regex::new(r"^\s*(\d+)(ms|s|m|h|d)?\s*$").expect("duration regex");
     let cap = re.captures(s).ok_or(InvalidDuration)?;
     let magnitude = cap[1].parse().map_err(|_| InvalidDuration)?;
